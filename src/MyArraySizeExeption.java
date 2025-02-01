90e5a6efbfb7d13cb306342207f99a9bf52babda
veronika <vopengame@gmail.com>
2025-02-01 15:08:40 +0300

    Finish

--- src/MyArraySizeExeption.java
+++ src/MyArraySizeExeption.java
@@ -2,11 +2,13 @@ class MyArraySizeExeption extends Exception {
     public MyArraySizeExeption(String message) {
         super(message);
     }
+
     public MyArraySizeExeption(String message, int cheсkSize) {
-        super(message+cheсkSize);
+        super(message + cheсkSize);
     }
-    public MyArraySizeExeption(String message1, int rowNumber, String message2, int cheсkSize) {
-        super(message1+rowNumber+message2+cheсkSize);
+
+    public MyArraySizeExeption(int rowNumber, int cheсkSize) {
+        super("в строке " + rowNumber + " количество столбцов != " + cheсkSize);
     }
 
 }
