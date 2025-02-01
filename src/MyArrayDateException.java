e4698a91a6e9cc09cdb7f691b8ca24048027f85f
veronika <vopengame@gmail.com>
2025-02-01 15:08:11 +0300

    finish

--- src/MyArrayDateException.java
+++ src/MyArrayDateException.java
@@ -2,7 +2,8 @@ public class MyArrayDateException extends Exception {
     public MyArrayDateException(String message) {
         super(message);
     }
+
     public MyArrayDateException(int i, int j, String message) {
-        super("в ячейке с индексами [" + i + "]" + "[" + j + "] " + message);
+        super("в ячейке [" + i + "]" + "[" + j + "] " + message);
     }
 }
